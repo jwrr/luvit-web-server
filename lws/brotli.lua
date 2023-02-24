ccf17f87a86e4a834759fecc0d1015b76e8f160b
jwrr <jwrr.com@gmail.com>
2023-02-24 00:14:05 -0500

    cache is starting to work

--- lws/brotli.lua
+++ lws/brotli.lua
@@ -7,7 +7,36 @@ local page = require'lws.page'
 local brotli = {}
 
 -- {sessionid = {userid, creation, last, expire}}
-brotli.cache = {}
+brotli.cache = { a = '1', b='2', c='3'}
+brotli.cache.path = '/var/www/html/cache'
+
+function brotli.cache.tostring()
+  return utils.tostring(brotli.cache, 0, 'brotlixxx = ')
+end
+
+function brotli.cache.add(body, url)
+  body = body or ''
+  url = url or page.urlFields.urlFileName or 'undefined'
+  if url ~= 'undefined' then
+    local filename = brotli.cache.path .. '/' .. url:gsub('/', '+')
+    local f = io.open(filename, "w")
+    f:write(body)
+    f:close()
+    brotli.cache[url] = filename
+  end
+end
+
+function brotli.cache.hit(url)
+  url = url or page.urlFields.urlFileName or 'undefined'
+  return brotli.cache[url] and true or false
+end
+
+function brotli.cache.get(res, url)
+  url = url or page.urlFields.urlFileName or 'undefined'
+  local filename = brotli.cache[url]
+  res:setHeader('Content-Encoding', 'br')
+  return utils.slurp(filename)
+end
 
 function brotli.accepted(headerString) 
   if (page and page.headers_t and page.headers_t['Accept-Encoding']) then
@@ -18,17 +47,14 @@ end
 
 brotli.compress = br.compress
 
+
 function brotli.compressIfAccepted(res, body)
   if brotli.accepted() then
     body = brotli.compress(body)
-    brotli.cache[page.url_t.path] = 1
     res:setHeader('Content-Encoding', 'br')
   end
+  brotli.cache.add(body)
   return body
-end
-
-function brotli.cache.tostring()
-  return utils.tostring(brotli.cache, 0, 'brotlixxx = ')
 end
 
 
--- lws/srv.lua
+++ lws/srv.lua
@@ -91,31 +91,41 @@ function srv.getBody(req, res)
   page.urlFields = srv.getUrlFields(page.sitePath, req.url)
   local body = ''
   if page.urlFields.fileFound then
-    if page.urlFields.fileType == 'lua' then
-      body = dofile(page.urlFields.fullPathName)
-      page.headers_t = srv.getHeaders(req)
-      page.url_t = url.parse(page.protocol .. '://' .. page.headers_t['Host'] .. req.url)
-      page.get_t = srv.getQuery()
-      page.add("method", req.method)
-      local page_str = utils.tostring(page, 0, "page = ")
-      local req_str = utils.tostring(req, 0, "req = ")
-      local res_str = utils.tostring(res, 0, "res = ")
-      local diag_str = page_str .. '\n\n\n' .. res_str .. '\n\n\n' .. req_str .. brotli.cache.tostring()
-      body = body .. "\n" .. diag_str
-    elseif page.urlFields.fileType == 'template' then
-      body = template.replace(req, res, page.urlFields, page.urlFields.fullPathName)
-    elseif page.urlFields.fileType == 'md' then
-      local markdownTemplateFile = page.sitePath .. '/templates/markdown.template'
-      body = utils.slurp(page.urlFields.fullPathName)
-      body = srv.convertMarkdown(req, res, page.urlFields, markdownTemplateFile, body)
+    page.headers_t = srv.getHeaders(req)
+    page.url_t = url.parse(page.protocol .. '://' .. page.headers_t['Host'] .. req.url)
+    page.get_t = srv.getQuery()
+    page.add("method", req.method)  
+    if brotli.cache.hit() then
+       body = brotli.cache.get(res)
     else
-      body = utils.slurp(page.urlFields.fullPathName)
+      if page.urlFields.fileType == 'lua' then
+        body = dofile(page.urlFields.fullPathName)
+        local page_str = utils.tostring(page, 0, "page = ")
+        local req_str = utils.tostring(req, 0, "req = ")
+        local res_str = utils.tostring(res, 0, "res = ")
+        local diag_str = page_str .. '\n\n\n' .. res_str .. '\n\n\n' .. req_str .. brotli.cache.tostring()
+        body = body .. "\n" .. diag_str
+      elseif page.urlFields.fileType == 'html' then
+        body = utils.slurp(page.urlFields.fullPathName)
+        local page_str = utils.tostring(page, 0, "page = ")
+        local req_str = utils.tostring(req, 0, "req = ")
+        local res_str = utils.tostring(res, 0, "res = ")
+        local diag_str = page_str .. '\n\n\n' .. res_str .. '\n\n\n' .. req_str .. brotli.cache.tostring()
+        body = body .. "\n" .. diag_str
+      elseif page.urlFields.fileType == 'template' then
+        body = template.replace(req, res, page.urlFields, page.urlFields.fullPathName)
+      elseif page.urlFields.fileType == 'md' then
+        local markdownTemplateFile = page.sitePath .. '/templates/markdown.template'
+        body = utils.slurp(page.urlFields.fullPathName)
+        body = srv.convertMarkdown(req, res, page.urlFields, markdownTemplateFile, body)
+      else
+        body = utils.slurp(page.urlFields.fullPathName)
+      end
+      body = brotli.compressIfAccepted(res, body)
     end
   else
     body = err.handler(req, res, page.urlFields, 404, page.sitePath)
   end
-
-  body = brotli.compressIfAccepted(res, body)
   mime.setHeader(res)
   res:setHeader('Content-Length', #body)
 
